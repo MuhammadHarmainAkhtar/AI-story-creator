7968e7ddcf2d4415f4af1bbd6b77ca9a0671c242
MrHarmain <mrharmain9@gmail.com>
2025-01-09 22:18:27 +0500

    so far

--- ai-story-generator/app/view-story/[id]/page.tsx
+++ ai-story-generator/app/view-story/[id]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client"
+import { StoryData } from "@/config/schema";
+import React  from "react";
+import { eq } from "drizzle-orm";
+import { db } from "@/config/db";
 
-const viewStory = () => {
+const viewStory = ({ params }: any) => {
+  const [story, setStory] = React.useState<any>();
+  React.useEffect(() => {
+    console.log(params.id);
+    getStory();
+  }, []);
+
+  const getStory = async () => {
+    const id = params.id;
+    const result = await db
+      .select()
+      .from(StoryData)
+      .where(eq(StoryData.storyId, id));
+    console.log(result[0]);
+    setStory(result[0]);
+  };
+  if (!story) return <p>Loading...</p>;
   return (
-    <div>viewStory</div>
-  )
-}
+    <div className="h-screen bg-black text-[#7D40EF]">
+      {" "}
+      <div className="p-10 md:px-20 lg:px-40">
+        <h1>{story?.output?.story_cover?.title}</h1>
+      </div>
+    </div>
+  );
+};
 
-export default viewStory
+export default viewStory;
